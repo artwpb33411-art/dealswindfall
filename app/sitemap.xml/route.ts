997ceb11fcc23176459b691d4ac68a7d85b9dd6c
artwpb33411-art <artwpb33411@gmail.com>
2025-12-09 18:45:12 -0500

    Fix formatting of deals fetching code

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -12,11 +12,11 @@ export async function GET() {
   /* -------------------------------------------------------------
       1. Fetch Deals
   ------------------------------------------------------------- */
- const { data: deals, error: dealsError } = await supabaseAdmin.rpc(
-  "get_all_deals"
-);
+const { data: deals, error: dealsError } =
+  await supabaseAdmin.rpc("get_all_deals");
 
 console.log("DEALS LENGTH:", deals?.length);
+
 console.log("DEALS ERROR:", dealsError);
 
 
